6012800192d8dd7f25556e2c99cd8b2ca51c79bf
Valencha <47866545@est.ort.edu.ar>
2023-08-03 15:10:50 -0300

    Finish DB

--- Models/BD.cs
+++ Models/BD.cs
@@ -25,13 +25,32 @@ static class BD {
         return _ObtenerDif;
     }
 
-    public static List<Preguntas> ObtenerPreguntas(int IdDificultad, int IdCategoria){
+    public static List<Preguntas> ObtenerPreguntas(int dificultad, int categoria){
+        dificultad = (dificultad == -1) ? null : dificultad;
+        categoria = (categoria == -1) ? null : categoria;
+
         List<Preguntas> _ObtenerPregs = new List<Preguntas>();
+
         using(SqlConnection db = new SqlConnection(_connectionString) ){
-            string SQL = "SELECT * FROM Dificultades";
-            _ObtenerPregs =  db.Query<Partido>(SQL).ToList();
+            string SQL = "SELECT * FROM Preguntas WHERE (@categoria IS NULL OR IdCategoria = @categoria) AND (@dificultad IS NULL OR IdDificultad = @dificultad)";
+            _ObtenerPregs =  db.Query<Partido>(SQL, new {dificultad = dificultad, categoria = categoria}).ToList();
         }
-        return _ObtenerDif;
+        return _ObtenerPregs;
     }
+
+
+    public static List<Respuestas> ObtenerRespuestas(List<Preguntas> preguntas){
+        List<Respuestas> _ObtenerRes = new List<Respuestas>();
+        foreach(var pregunta in preguntas){
+            var temp;
+            using(SqlConnection db = new SqlConnection(_connectionString) ){
+                string SQL = "SELECT * FROM Respuestas WHERE IdPregunta = @IdPreg";
+                temp =  db.QueryFirstOrDefault<Partido>(SQL, new {IdPreg = pregunta.IdPregunta}).ToList();
+            }
+            _ObtenerRes.AddRange(temp);
+        }
+        return _ObtenerRes;
+    }
+
 
 }
